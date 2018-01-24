1a2d4af02a716e79a54ee791b99e2c72c5ffadd3
Camila Daniels <camila.daniels926@gmail.com>
2018-01-23 18:46:22 -0600

    Camila Daniels camila.daniels926@gmail.com I relied heavily on the tutorials on this website (https://redux.js.org/docs/basics/ExampleTodoList.html) to complete this assignment. It still won't compile. I am working on these tutorials (https://egghead.io/courses/getting-started-with-redux) now and will re-attempt tonight.

---
+++ todo/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import todoApp from './reducers';
+import App from './components/App';
+
+let store = createStore(todoApp);
+
+render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+)
+
+// render() {
+// 	return (
+// 		<Provider store={store}>
+// 			<App />
+// 		</Provider>,
+// 		document.getElementById('root')
+// 	);
+// }
